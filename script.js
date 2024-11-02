cb5d18d0dcf1a2658d0017b6bbbf9794b436a2a9
ThembiTech <thembisaskosana785@gmail.com>
2024-11-02 18:24:00 +0200

    Injected forecast html from js file

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ function displayTemperature(response){
     let temperatureElement = document.querySelector("#current-temperature");
     let temperature = Math.round(response.data.temperature.current);
     temperatureElement.innerHTML = temperature;
-    
+
     let cityElement = document.querySelector("#current-city");
     console.log(response.data);
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind_speed");
     let currentDateELement = document.querySelector("#current-date");
-   
+    let iconElement=document.querySelector("#icon");
+
     let date= new Date(response.data.time*1000);
     let hours= date.getHours().toString().padStart(2,'0');
     let minutes= date.getMinutes().toString().padStart(2,'0');
     let formattedDate=formatDate(date);
+   
+
+    iconElement.innerHTML= `<img src= "${response.data.condition.icon_url}" class="current-tempererature-icon" alt="${response.data.condition.description}"/>`;
    
     cityElement.innerHTML=response.data.city;
     descriptionElement.innerHTML= response.data.condition.description;
@@ -64,3 +68,41 @@ let currentDate = new Date();
 let currentHours=currentDate.getHours.toString().padStart(2,'0');
 let currentMinutes=currentDate.getMinutes.toString().padStart(2,'0');
 currentDateELement.innerHTML = `${formatDate(currentDate)}, ${currentHours}:${currentMinutes}`;
+
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
+displayForecast();
